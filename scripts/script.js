710fdbe16336d872700a222bb21d7bc1fa820052
ljmxjh <50219045+ljmxjh@users.noreply.github.com>
2021-02-11 00:38:18 +0800

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  fetch("https://fakestoreapi.com/products")
+  .then(response => response.json())
+  .then(data => window.localStorage.setItem("arrays", JSON.stringify(data)));
+
+
+  
 });
